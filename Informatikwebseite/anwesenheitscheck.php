b2850f4fe86c7478019f52ae96249e8f35772e81
valentin829 <144284258+valentin829@users.noreply.github.com>
2023-11-07 22:56:45 +0100

    anwesenheitscheck.php

--- Informatikwebseite/anwesenheitscheck.php
+++ Informatikwebseite/anwesenheitscheck.php
@@ -1,7 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Datenbankverbindung herstellen
     $dbHost = 'localhost';
     $dbUser = 'root';
     $dbPassword = '';
@@ -9,22 +8,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
 
-    // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
     if ($conn->connect_error) {
         die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
     }
 
-    // Daten aus dem Formular sicher abrufen und bereinigen
     $childName = mysqli_real_escape_string($conn, $_POST['child_name']);
     $date = mysqli_real_escape_string($conn, $_POST['date']);
     $course = mysqli_real_escape_string($conn, $_POST['course']);
     $isPresent = isset($_POST['present']) ? 1 : 0;
 
-    // Validierung der Daten (kann weiter angepasst werden)
     if (empty($childName) || empty($date) || empty($course)) {
         echo "Bitte alle erforderlichen Informationen eingeben.";
     } else {
-        // SQL-Abfrage zum Einfügen der Daten
         $sql = "INSERT INTO attendance (child_name, date, course, is_present) VALUES ('$childName', '$date', '$course', $isPresent)";
 
         if ($conn->query($sql) === TRUE) {
